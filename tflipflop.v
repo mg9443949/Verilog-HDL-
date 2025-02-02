ae4c1b57b275b437f6578d2b95cb241329508172
Mourya G <143474645+mg9443949@users.noreply.github.com>
2025-02-02 12:29:07 +0530

    Create tflipflop.v

---
+++ tflipflop.v
@@ -0,0 +1,19 @@
+module tff(clk,rst,t,q,qn);
+  input clk,rst,t;
+  output reg q;
+  output qn;
+  always @ (posedge clk or posedge rst) begin
+    if(rst) begin
+      t<=0;
+    end
+    else  begin
+      if (t) begin
+        q<=~q;
+      end
+      else if (!t) begin
+        q<=q;
+      end
+    end
+  end
+  assign qn=~q;
+endmodule
